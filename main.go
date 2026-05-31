94ff595e0199ae65ba1ec777568a300ce736f6c3
abdullahbasir <abdullah.sbasir@gmail.com>
2026-05-31 17:40:32 +0400

    Created a custom handler

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 
 	ServeMux := http.NewServeMux()
 
-	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	ServeMux.HandleFunc("/healthz", handlerReadiness)
+
+	ServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	serverStruct := &http.Server{
 		Addr:    ":8080",
@@ -17,5 +25,4 @@ func main() {
 	}
 
 	log.Fatal(serverStruct.ListenAndServe())
-
 }
