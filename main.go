a2cfeca078b62384de276bd4cec087f5f9fa9a78
abdullahbasir <abdullah.sbasir@gmail.com>
2026-05-31 22:30:45 +0400

    Added middleware handlers that count and reset number of requests

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
@@ -11,13 +13,42 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) HandleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hit_num := cfg.fileserverHits.Load()
+	resp := fmt.Sprintf("Hits: %d", hit_num)
+	w.Write([]byte(resp))
+}
+
+func (cfg *apiConfig) HandleReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
 
 	ServeMux := http.NewServeMux()
 
-	ServeMux.HandleFunc("/healthz", handlerReadiness)
+	apiCfg := &apiConfig{}
 
-	ServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	ServeMux.Handle("/app/", apiCfg.middleware(handler))
+	ServeMux.HandleFunc("/healthz", handlerReadiness)
+	ServeMux.HandleFunc("/metrics", apiCfg.HandleMetrics)
+	ServeMux.HandleFunc("/reset", apiCfg.HandleReset)
 
 	serverStruct := &http.Server{
 		Addr:    ":8080",
