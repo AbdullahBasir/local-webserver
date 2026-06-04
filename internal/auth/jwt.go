2a63e9f56221ba865d2d1472ae301dd8be2ea7a1
abdullahbasir <abdullah.sbasir@gmail.com>
2026-06-04 07:06:27 +0400

    Added jwt authentication to the auth package

---
+++ internal/auth/jwt.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	key := []byte(tokenSecret)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	tokenString, err := claims.SignedString(key)
+	return tokenString, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	} else if access, ok := token.Claims.(*jwt.RegisteredClaims); ok {
+		convert, err := uuid.Parse(access.Subject)
+		if err != nil {
+			return uuid.Nil, err
+		}
+		return convert, nil
+	}
+	return uuid.Nil, errors.New("unknown claims type, cannot proceed")
+}
