54780feb3078ae57cfff5bb73540d35a38056af0
abdullahbasir <abdullah.sbasir@gmail.com>
2026-06-05 07:03:37 +0400

    Added the Creat refresh token inside the user login

--- user_login.go
+++ user_login.go
@@ -6,22 +6,23 @@ import (
 	"time"
 
 	"github.com/AbdullahBasir/local-webserver/internal/auth"
+	"github.com/AbdullahBasir/local-webserver/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) HandleUserLogin(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Password           string `json:"password"`
-		Email              string `json:"email"`
-		expires_in_seconds int
+		Password string `json:"password"`
+		Email    string `json:"email"`
 	}
 
-	type userToken struct {
-		ID        uuid.UUID `json:"id"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Email     string    `json:"email"`
-		Token     string    `json:"token"`
+	type responseBody struct {
+		Id           uuid.UUID `json:"id"`
+		CreatedAt    time.Time `json:"created_at"`
+		UpdatedAt    time.Time `json:"updated_at"`
+		Email        string    `json:"email"`
+		Token        string    `json:"token"`
+		RefreshToken string    `json:"refresh_token"`
 	}
 
 	params := parameters{}
@@ -44,21 +45,27 @@ func (cfg *apiConfig) HandleUserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	expire := 0
-	if params.expires_in_seconds != 0 {
-		expire = params.expires_in_seconds * int(time.Second)
-	}
-	expire = 1 * int(time.Hour)
+	exp := 60 * (24 * time.Hour)
+	refreshToken := auth.MakeRefreshToken()
+
+	rToken, err := cfg.dbQueries.CreateRefreshToken(r.Context(), database.CreateRefreshTokenParams{
+		Token:     refreshToken,
+		UserID:    user.ID,
+		ExpiresAt: time.Now().Add(exp),
+	})
+
+	expire := 1 * int(time.Hour)
 	jwt, err := auth.MakeJWT(user.ID, cfg.Secret, time.Duration(expire))
 	if err != nil {
 		respondWithError(w, 500, "Internal Server Error")
 	}
 
-	respondWithJSON(w, 200, userToken{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
-		Token:     jwt,
+	respondWithJSON(w, 200, responseBody{
+		Id:           rToken.UserID,
+		CreatedAt:    user.CreatedAt,
+		UpdatedAt:    user.UpdatedAt,
+		Email:        user.Email,
+		Token:        jwt,
+		RefreshToken: rToken.Token,
 	})
 }
