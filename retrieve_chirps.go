62ee4013a85e3b748711f7b48352f50202cc48fb
abdullahbasir <abdullah.sbasir@gmail.com>
2026-06-07 10:59:43 +0400

    Added a function to retrieve chirps based on the users id

--- retrieve_chirps.go
+++ retrieve_chirps.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) RetrieveChirps(w http.ResponseWriter, r *http.Request) {
+	author := r.URL.Query().Get("author_id")
+	parseID, err := uuid.Parse(author)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID")
+		return
+	}
+
+	if author != "" {
+		authorChirp, err := cfg.dbQueries.GetAuthorChirps(r.Context(), parseID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Failed to retrieve chirps")
+			return
+		}
+		respBody := make([]Chirp, len(authorChirp))
+		for i, author := range authorChirp {
+			respBody[i] = Chirp{
+				ID:        author.ID,
+				CreatedAt: author.CreatedAt,
+				UpdatedAt: author.UpdatedAt,
+				Body:      author.Body,
+				UserID:    author.UserID,
+			}
+		}
+		respondWithJSON(w, http.StatusOK, respBody)
+		return
+	}
+
 	chirps, err := cfg.dbQueries.GetChirps(r.Context())
 	if err != nil {
-		respondWithError(w, 500, "Failed to retrieve chirps")
+		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve chirps")
 		return
 	}
 	respBody := make([]Chirp, len(chirps))
@@ -20,5 +49,5 @@ func (cfg *apiConfig) RetrieveChirps(w http.ResponseWriter, r *http.Request) {
 			UserID:    chirp.UserID,
 		}
 	}
-	respondWithJSON(w, 200, respBody)
+	respondWithJSON(w, http.StatusOK, respBody)
 }
