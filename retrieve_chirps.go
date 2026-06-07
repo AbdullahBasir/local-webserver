cbd92b6c42ef3e85b9b44fe8f31c7bd04a001da5
abdullahbasir <abdullah.sbasir@gmail.com>
2026-06-07 13:00:41 +0400

    Cleaned the get chirps handler and added a sorting feature

--- retrieve_chirps.go
+++ retrieve_chirps.go
@@ -2,52 +2,59 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
+	"github.com/AbdullahBasir/local-webserver/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) RetrieveChirps(w http.ResponseWriter, r *http.Request) {
-	author := r.URL.Query().Get("author_id")
-	parseID, err := uuid.Parse(author)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID")
-		return
-	}
+	var parseID uuid.UUID
+	var err error
 
+	author := r.URL.Query().Get("author_id")
 	if author != "" {
-		authorChirp, err := cfg.dbQueries.GetAuthorChirps(r.Context(), parseID)
+		parseID, err = uuid.Parse(author)
 		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Failed to retrieve chirps")
+			respondWithError(w, http.StatusBadRequest, "Invalid chirp ID")
 			return
 		}
-		respBody := make([]Chirp, len(authorChirp))
-		for i, author := range authorChirp {
-			respBody[i] = Chirp{
-				ID:        author.ID,
-				CreatedAt: author.CreatedAt,
-				UpdatedAt: author.UpdatedAt,
-				Body:      author.Body,
-				UserID:    author.UserID,
-			}
-		}
-		respondWithJSON(w, http.StatusOK, respBody)
-		return
 	}
 
-	chirps, err := cfg.dbQueries.GetChirps(r.Context())
+	var dbChirps []database.Chirp
+
+	if parseID != uuid.Nil {
+		dbChirps, err = cfg.dbQueries.GetAuthorChirps(r.Context(), parseID)
+	} else {
+		dbChirps, err = cfg.dbQueries.GetChirps(r.Context())
+	}
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve chirps")
 		return
 	}
-	respBody := make([]Chirp, len(chirps))
-	for i, chirp := range chirps {
-		respBody[i] = Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
-		}
+	respBody := []Chirp{}
+	for _, dbchirp := range dbChirps {
+		respBody = append(respBody, Chirp{
+			ID:        dbchirp.ID,
+			CreatedAt: dbchirp.CreatedAt,
+			UpdatedAt: dbchirp.UpdatedAt,
+			Body:      dbchirp.Body,
+			UserID:    dbchirp.UserID,
+		})
 	}
-	respondWithJSON(w, http.StatusOK, respBody)
+
+	sorting := r.URL.Query().Get("sort")
+
+	if sorting == "desc" {
+		sort.Slice(respBody, func(i, j int) bool {
+			return respBody[i].CreatedAt.After(respBody[j].CreatedAt)
+		})
+		respondWithJSON(w, http.StatusOK, respBody)
+		return
+	} else {
+		sort.Slice(respBody, func(i, j int) bool {
+			return respBody[i].CreatedAt.Before(respBody[j].CreatedAt)
+		})
+		respondWithJSON(w, http.StatusOK, respBody)
+	}
 }
